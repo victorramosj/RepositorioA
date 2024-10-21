8d6b29639e7459f4ad6ed36928cc3d094583067b
victorramosj <48687982+victorramosj@users.noreply.github.com>
2024-10-21 20:22:12 -0300

    mudança

--- microblog/app.py
+++ microblog/app.py
@@ -1,13 +1,14 @@
 from flask import Flask
 app = Flask(__name__)
+
+@app.route('/contato')
+def index():
+    return "email@mail.com"
+
 @app.route('/')
 def contato():
-    return "email@mail.com"
-@app.route('/')
-def index():
+    
     return 'Olá mundo!'
-
-
 
 if __name__ == '__main__':
     app.run()

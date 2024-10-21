b87fdc675e26139f52187d0efc787da78ecaa9a8
victorramosj <48687982+victorramosj@users.noreply.github.com>
2024-10-21 19:56:13 -0300

    muda

--- microblog/.flaskenv
+++ microblog/.flaskenv
@@ -1,2 +1,2 @@
-FLASK/-APP=app.py
+FLASK_APP=app.py
 FLASK_ENV=development
--- microblog/app.py
+++ microblog/app.py
@@ -1,13 +1,13 @@
 from flask import Flask
 app = Flask(__name__)
-
 @app.route('/')
-def index():
-    return'Olá mundo!'
-
-@app.route('/contato')
 def contato():
     return "email@mail.com"
+@app.route('/')
+def index():
+    return 'Olá mundo!'
+
+
 
 if __name__ == '__main__':
     app.run()

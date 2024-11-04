58535c1f5ea63e291115d7e6e960c5869eed3e2b
victorramosj <48687982+victorramosj@users.noreply.github.com>
2024-11-04 18:58:47 -0300

    contato mudanças

--- microblog/app.py
+++ microblog/app.py
@@ -1,3 +1,7 @@
+#python -m venv venv
+#.\venv\Scripts\activate
+
+
 from flask import Flask, render_template
 
 app = Flask(__name__)
@@ -8,7 +12,7 @@ def index():
 
 @app.route('/contato')
 def contato():
-  return render_template('contato.html')
+  return render_template('contato.html', tel='(87) 99898-9898')
 
 
 if __name__ == '__main__':

4e742130b18abbccd0938c02c393fea8d9cc9d4d
victorramosj <48687982+victorramosj@users.noreply.github.com>
2024-11-04 21:20:01 -0300

    asd

--- microblog/app.py
+++ microblog/app.py
@@ -23,8 +23,18 @@ def loja():
   return render_template('loja.html', )
 @app.route('/usuario')
 @app.route('/usuario/<nome>')
-def index(nome="usuario comum"):
-    return nome
+def usuario(nome="usuario comum"):
+    return  "olá "+ nome + "!"
+
+@app.route('/operacoes')
+@app.route('/operacoes/<int:n1>/<int:n2>')
+def somar(n1 = 0, n2 = 1):
+  resultadosoma = n1 + n2
+  resultadosub = n1 - n2
+  resultadomult = n1 * n2
+  resultadodiv = n1 / n2
+  return render_template('operacoes.html', n1=n1, n2=n2, resultadosoma=resultadosoma, resultadosub=resultadosub, resultadomult=resultadomult, resultadodiv=resultadodiv)
+
 
 
 
